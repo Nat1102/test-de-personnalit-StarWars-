c34ae2e6fd1583e1c438e7ad91be9b369752b6c2
Nat00 <natpiette1102@gmail.com>
2026-05-23 20:22:52 +0200

    Update question.js

--- script/question.js
+++ script/question.js
@@ -111,4 +111,4 @@ const FIN = {
 }
 
 //tableaux de question
-const listeQuestion =[question1, question2, question3, FIN]
+const listeQuestion =[question1, question2, question3,question4, question5, question6, question7, question8, FIN]
